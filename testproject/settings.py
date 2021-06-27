4a15d8cff215253a29ca8b13e98ecf4a157c1857
zahqresh <muhammadhamza2965@gmail.com>
2021-06-27 07:35:30 +0500

    fixing gunicorn error

--- testproject/settings.py
+++ testproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4-t(gbmkb0n4)1bvq2act@w!zippn2svu^lf2z5rdtfn#es^(2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ourtestproject.herokuapp.com']
 
 
 # Application definition
